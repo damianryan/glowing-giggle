df723f10b7f0cc55260eabcb88ec03ba73141055
Damian Ryan <github@damianryan.com>
2018-05-21 20:57:28 +0100

    Corrected error string for scraper

--- src/main/kotlin/com.damianryan.scrape/Main.kt
+++ src/main/kotlin/com.damianryan.scrape/Main.kt
@@ -2,11 +2,11 @@ package com.damianryan.scrape
 
 fun main(args: Array<String>) {
     if (args.size < 1) {
-        System.err.println("Usage: java -jar scraper.jar <URL> [maxDepth]")
+        System.err.println("Usage: java -jar scraper.jar <URL> [maxThreads] [maxDepth]")
         System.exit(1)
     }
     val url = args[0]
-    val maxThreads = if (args.size > 1) args[1].toInt() else 2
+    val maxThreads = if (args.size > 1) args[1].toInt() else 3
     val maxDepth = if (args.size > 2) args[2].toInt() else -1
     Crawler(JSoapLinkLoader(), maxThreads).crawl(url, maxDepth)
 }
